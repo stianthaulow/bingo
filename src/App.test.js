becdc2c6fdc9aa9ea6d97c8e00d4ca2ca4794d0c
Stian Thaulow <stian@thaulow.me>
2020-03-23 20:47:34 +0100

    Update test

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders main content with title", () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const linkElement = getByText(/Møtebingo/i);
   expect(linkElement).toBeInTheDocument();
 });
