8c42f2497ff719e2177ed95b473c9e70825ebe62
Stian Thaulow <stian@thaulow.me>
2020-03-23 07:01:01 +0100

    Updates

---
+++ precache-manifest.78e53a056869a90f947d0a8b2d709de8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07f562a79cf4306f69e223f89d1ac776",
+    "url": "/bingo/index.html"
+  },
+  {
+    "revision": "07dd02044c1116d5a776",
+    "url": "/bingo/static/css/main.3048a913.chunk.css"
+  },
+  {
+    "revision": "c1276fcdf335843a8124",
+    "url": "/bingo/static/js/2.c2aaa7d7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/bingo/static/js/2.c2aaa7d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07dd02044c1116d5a776",
+    "url": "/bingo/static/js/main.8c11337c.chunk.js"
+  },
+  {
+    "revision": "3e96d5cca2553c021ce2",
+    "url": "/bingo/static/js/runtime-main.d558581a.js"
+  }
+]);
